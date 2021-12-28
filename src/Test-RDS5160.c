67b2a9aa46ab08b02216e034d54a94af858e4b14
Christopher Coballes <cobecoballes@gmail.com>
2021-12-28 22:32:04 +0800

    Update Test-RDS5160.c

--- src/Test-RDS5160.c
+++ src/Test-RDS5160.c
@@ -3,6 +3,26 @@
   pca9685-servomotor-demo.ino
   Demonstrates use of 16 channel I2C PWM driver board with 4 servo motors
   Uses Adafruit PWM library
+  RDS5160 Description:
+
+ Brand: DSSERVO
+ Item: RDS5160 Digital Servo
+ Type: 180°/ 270°
+ Operating Voltage: 6.0-8.4 DC Volts
+ Stall Current: 3.5A/5A/6.2A(6.0V/7.4V/8.4V)
+ Torque: 58kg.cm(6.0V), 65kg.cm(7.4V), 70kg.cm(8.4V)
+ Speed: 0.17sec/60°(6.0V), 0.15sec/60°(7.4V), 0.13sec/60°(8.4V)
+ Pulse: 500-2500us
+ Frequency: 50-333Hz
+ Dead Band: 3us
+ Neutrol Position: 1500us
+ Control: PWM
+ Rotation: 180°/ 270°Counterlockwise
+ Gear: Metal Gear
+ Teeth: 25T
+ Wire: 300mm
+ Servo Dimention: 60*30*48 mm
+ 
 Hi-Techno Barrio
 */
 
